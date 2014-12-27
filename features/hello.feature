64453fcf7061db6dba25dae7f863a028995dfaee
cskksc <ckoparkar@live.in>
2014-12-27 12:01:42 +0530

    Remove hello.feature

--- features/hello.feature
+++
@@ -1,5 +0,0 @@
-Feature: Say hello world
-
-  Scenario: hello sir
-    Given nothing
-    Then I should get hello world
--- src/game_of_life_clj/core.clj
+++ src/game_of_life_clj/core.clj
@@ -1,8 +1,6 @@
 (ns game-of-life-clj.core
   (:gen-class))
 
-(defn hello_world []
-  "hello_world")
 
 (defn -main
   "I don't do a whole lot ... yet."
